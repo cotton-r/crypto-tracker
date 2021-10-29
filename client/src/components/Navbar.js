5bebe7e55f427b8418e4fa5498f5ee7b8e7b694b
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2021-10-29 23:58:46 +0100

    create navbar, apply styles, import fonts

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
-import { HomeOutlined, MoneyCollectedOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+import { HomeOutlined, MoneyCollectedOutlined, BulbOutlined, DollarOutlined, MenuOutlined, LineChartOutlined } from '@ant-design/icons';
+
+import './Navbar.css';
 
 import icon from '../images/crypto-icon.png';
 
@@ -10,13 +12,18 @@ const Navbar = () => {
         <div className='nav-container'>
             <div className='logo-container'>
                 <Avatar src={icon} size='large' />
-                <Typography.Title level={2} className='logo'>
-                    <Link to='/'>Crypto Tracker</Link>
-                </Typography.Title>
-                {/* <Button className='menu-control-container'>
-
-                </Button> */}
             </div>
+            <Menu theme='dark'>
+                <Menu.Item icon={<HomeOutlined />} className='nav-item'>
+                    <Link to='/' className='nav-item-icon'>Dashboard</Link>
+                </Menu.Item>
+                <Menu.Item icon={<DollarOutlined />} className='nav-item'>
+                    <Link to='/cryptocurrencies' className='nav-item-icon'>Cryptocurrencies</Link>
+                </Menu.Item>
+                <Menu.Item icon={<LineChartOutlined />} className='nav-item'>
+                    <Link to='/news' className='nav-item-icon'>News</Link>
+                </Menu.Item>
+            </Menu>
         </div>
     )
 }
