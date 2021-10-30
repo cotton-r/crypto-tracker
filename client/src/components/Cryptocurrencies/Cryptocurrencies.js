4228cb7afb388d08820da33ec57055f8799d4067
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2021-10-30 19:46:11 +0100

    set up basic layout for all 3 routes

--- client/src/components/Cryptocurrencies/Cryptocurrencies.js
+++ client/src/components/Cryptocurrencies/Cryptocurrencies.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Cryptocurrencies = () => {
     return (
-        <div>
+        <div className='page-container'>
             Cryptocurrencies
         </div>
     )
