95bb66c4796b630f90cb4773bbf0af0e4c3efc6e
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2021-10-30 23:34:29 +0100

    fetch data from api and create global stats tile

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -3,21 +3,32 @@ import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
+import './Dashboard.css';
+
 import { useGetCryptosQuery } from '../../services/cryptoApi';
 
 const { Title } = Typography;
 
 const Dashboard = () => {
+
+    const { data, isFetching } = useGetCryptosQuery();
+    const globalStats = data?.data?.stats;
+
+    console.log(data);
+
+    if (isFetching) return 'Loading...';
+
     return (
         <div className='page-container'>
-            <Title level={2} className='heading'>Global Crypto Stats</Title>
-            <Row>
-                <Col span={12}><Statistic title='Total Cryptocurrenices' value='5' /></Col>
-                <Col span={12}><Statistic title='Total Exchanges' value='5' /></Col>
-                <Col span={12}><Statistic title='Total Market Cap' value='5' /></Col>
-                <Col span={12}><Statistic title='Total Total 24hr Volume' value='5' /></Col>
-                <Col span={12}><Statistic title='Total Markets' value='5' /></Col>
-            </Row>
+            <div className='global-stats-tile'>
+                <Title level={2} className='global-stats-heading'>Global Crypto Stats</Title>
+                <Row>
+                    <Col span={12}><Statistic title='Total Cryptocurrenices' value={globalStats.total} /></Col>
+                    <Col span={12}><Statistic title='Total Exchanges' value={millify(globalStats.totalExchanges)} /></Col>
+                    <Col span={12}><Statistic title='Total Market Cap' value={millify(globalStats.totalMarketCap)} /></Col>
+                    <Col span={12}><Statistic title='Total 24hr Volume' value={millify(globalStats.total24hVolume)} /></Col>
+                </Row>
+            </div>
         </div>
     )
 };
