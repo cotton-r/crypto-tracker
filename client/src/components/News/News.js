9a702dbe671773bbce720a8b3fd8344dda673b87
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2021-11-01 23:30:35 +0000

    no changes

--- client/src/components/News/News.js
+++ client/src/components/News/News.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
+import moment from 'moment';
 
 const News = () => {
     return (
-        <div className='page-container'>
+        <div className='page-container news-page'>
             News
         </div>
     )
