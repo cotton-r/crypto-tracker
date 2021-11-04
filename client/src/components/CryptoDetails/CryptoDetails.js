272fac4132b985cf0ee7db170a6e844ded54bebb
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2021-11-04 18:56:33 +0000

    change styling of details page

--- client/src/components/CryptoDetails/CryptoDetails.js
+++ client/src/components/CryptoDetails/CryptoDetails.js
@@ -130,6 +130,21 @@ const CryptoDetails = ({ userCurrency }) => {
                                 ))}
                             </div>
                         </Col>
+                        <Col className='coin-links'>
+                            <Title level={2} className='coin-details-heading'>
+                                {cryptoDetails?.name} Links
+                            </Title>
+                            {cryptoDetails?.links.map((link) => (
+                                <Row className='coin-link' key={link?.name}>
+                                    <Title level={5} className='link-name'>
+                                        {link?.type}
+                                    </Title>
+                                    <a href={link?.url} target='_blank' rel='noreferrer'>
+                                        {link?.name}
+                                    </a>
+                                </Row>
+                            ))}
+                        </Col>
                     </Col>
                     <Col className='coin-desc-link'>
                         <Row className='coin-desc'>
@@ -140,21 +155,6 @@ const CryptoDetails = ({ userCurrency }) => {
                         </Row>
                     </Col>
                 </div>
-                <Col className='coin-links'>
-                        <Title level={2} className='coin-details-heading'>
-                            {cryptoDetails?.name} Links
-                        </Title>
-                        {cryptoDetails?.links.map((link) => (
-                            <Row className='coin-link' key={link?.name}>
-                                <Title level={5} className='link-name'>
-                                    {link?.type}
-                                </Title>
-                                <a href={link?.url} target='_blank' rel='noreferrer'>
-                                    {link?.name}
-                                </a>
-                            </Row>
-                        ))}
-                    </Col>
             </Col>
         </div>
     )
